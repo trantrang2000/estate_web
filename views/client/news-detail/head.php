a47883b3cbaa7545b3f13fd88a9828916c3aeb7d
trantrang2000 <internship@gmail.com>
2021-05-06 21:12:13 +0700

    update news detail

---
+++ views/client/news-detail/head.php
@@ -0,0 +1,6 @@
+<title>Trang tin tức chi tiết</title>
+<link rel="stylesheet" href="https://unpkg.com/swiper/swiper-bundle.min.css" />
+<link rel="stylesheet" href="public/client/src/css/news-detail.css" />
+
+<script defer src="https://unpkg.com/swiper/swiper-bundle.min.js"></script>
+<script defer src="public/client/src/js/news-detail.js"></script>
